270c6ce2bf899d910209131a9ad8f53f26be53c9
BlenderZegarek <theeras.terebecki@gmail.com>
2023-09-24 18:32:19 +0200

    Update1

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function httpGet(url) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", url, false);
-    xmlHttp.send(null);
-    return xmlHttp.responseText;
+    var xmlHttp = new XMLHttpRequest()
+    xmlHttp.open("GET", url, false)
+    xmlHttp.send(null)
+    return xmlHttp.responseText
 }
 
 window.onload = function () {
@@ -13,7 +13,25 @@ window.onload = function () {
     const projectsgrid = document.getElementById("projectsgrid")
     const loading = document.getElementById('loading')
 
+    const archiveicon = '<p projectbadge><i class="bi bi-archive-fill"></i></p>'
+    const discussicon = '<p projectbadge><i class="bi bi-chat-right-text-fill"></i></p>'
+    const pagesicon = '<p projectbadge><i class="bi bi-globe"></i></p>'
+    const wikiicon = '<p projectbadge><i class="bi bi-book-fill"></i></p>'
     repos.forEach(element => {
+
+        let badges = ""
+        if (element.archived) {
+            badges += archiveicon
+        }
+        if (element.has_discussions) {
+            badges += discussicon
+        }
+        if (element.has_pages) {
+            badges += pagesicon
+        }
+        if (element.has_wiki) {
+            badges += wikiicon
+        }
 
         const card = `
             <a id="project1" class="projectmain" href="${element.html_url}">
@@ -24,6 +42,7 @@ window.onload = function () {
                         <p class="projectstars"><i class="bi bi-star-fill"></i> ${element.stargazers_count}</p>
                         <p class="projectlang"><i class="bi bi-code-slash"></i> ${element.language}</p>
                         <p class="projectviewers"><i class="bi bi-eye-fill"></i> ${element.watchers_count}</p>
+                        ${badges}
                     </div>
                 </div>
             </a>
@@ -55,7 +74,7 @@ window.onload = function () {
             const yRotation = 7 * ((xVal - width / 2) / width)
             const xRotation = -7 * ((yVal - height / 2) / height)
             
-            const string = "perspective(500px) rotateX(" + xRotation + "deg) scale(1.05) rotateY(" + yRotation + "deg)"
+            const string = "perspective(500px) rotateX(" + xRotation + "deg) scale(1.03) rotateY(" + yRotation + "deg)"
             
             el.style.transform = string
             
@@ -68,11 +87,11 @@ window.onload = function () {
         })
 
         el.addEventListener("mousedown", function() {
-            el.style.transform = 'perspective(500px) scale(0.95) rotateX(0) rotateY(0)'
+            el.style.transform = 'perspective(500px) scale(0.97) rotateX(0) rotateY(0)'
         })
 
         el.addEventListener("mouseup", function() {
-            el.style.transform = 'perspective(500px) scale(1.05) rotateX(0) rotateY(0)'
+            el.style.transform = 'perspective(500px) scale(1.03) rotateX(0) rotateY(0)'
         })
         
     }
